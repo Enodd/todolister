7e6cbf8ce0328dda879f3d4b9584b09c49ff03c8
Enodd <foxnacity@gmail.com>
2024-10-18 18:36:52 +0200

    feat: base setup

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod models;
+pub mod services;
+pub mod controllers;
+pub mod repositories;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,37 @@
+use std::env;
+
+use axum::{routing::get, Router};
+use diesel::{r2d2::{ConnectionManager, Pool}, PgConnection};
+use dotenvy::dotenv;
+use tokio::net::TcpListener;
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
+use todolister::models::app_state::{AppState, DbPool};
+
+pub type Error = Box<dyn std::error::Error>;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    let database_url: String = env::var("DATABASE_URL").expect("database url must be set in environment");
+    let connection_manager: ConnectionManager<PgConnection> = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: DbPool = Pool::builder().build(connection_manager).expect("Failed to create connection pool");
+
+    let app_state = AppState {
+        pool
+    };
+
+    let app = Router::new()
+        .layer(
+            ServiceBuilder::new()
+                .layer(CorsLayer::permissive())
+        )
+        .route("/", get(|| async { "hello world" }))
+        .with_state(app_state);
+
+    let listener : TcpListener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
     Ok(())
 }
